6276d0e539576277416c9a2786d39df915c2a822
sean112533 <zdte141@live.rhul.ac.uk>
2021-05-30 01:18:43 +0100

    update the new question reverseInteger

---
+++ JavaPractice/src/test/TestReverseInteger.java
@@ -0,0 +1,25 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import interviewQuestions.ReverseInteger;
+
+public class TestReverseInteger {
+
+  @Before
+  public void testClass() {
+    ReverseInteger reverseInteger = new ReverseInteger();
+
+  }
+
+  @Test
+  public void testReverseInteger() {
+    int input = 123;
+    int output = 321;
+    ReverseInteger test = new ReverseInteger();
+
+    assertTrue("check for reverse of the integer is correct", test.reverse(input) == output);
+  }
+
+}
