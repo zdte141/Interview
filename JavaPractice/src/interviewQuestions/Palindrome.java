8044ee8de4e463fa23d5359ee78501ca7e9a5495
sean112533 <zdte141@live.rhul.ac.uk>
2021-05-31 01:38:34 +0100

    another chekcout

--- JavaPractice/src/interviewQuestions/Palindrome.java
+++ JavaPractice/src/interviewQuestions/Palindrome.java
@@ -10,18 +10,28 @@ package interviewQuestions;
  *
  */
 public class Palindrome {
-
+  /**
+   * method palindrome to revert the integer and checks for the correct boolean result.
+   * 
+   * @param input of the integer
+   * @return the boolean true or false.
+   */
   public boolean palindrome(int input) {
+    // if the input is less than 0 it's not palidrome number.
+    // to be palindrome number first and last digit of the number have to be 0.
+    // example 020.
     if (input < 0 || (input % 10 == 0 && input != 0)) {
       return false;
     }
-
+    // implemented method from the reverseInteger, set the inital integer value by 0.
+    // if the input is bigger than the initial value, which means not all number been reversed.
+    // reverse number is being reverted until it reverse it's value.
     int reverseNum = 0;
     while (input > reverseNum) {
       reverseNum = reverseNum * 10 + input % 10;
       input = input / 10;
     }
-
+    // return true only if the reversed value is same to the input value.
     return input == reverseNum || input == reverseNum / 10;
   }
 
