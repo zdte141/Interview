4d33526b6f24b460df07da7fd745475b0444c087
sean112533 <zdte141@live.rhul.ac.uk>
2021-06-24 21:33:49 +0100

    commit the InOrderTraversal

--- JavaPractice/src/interviewQuestions/MaxDepth.java
+++ JavaPractice/src/interviewQuestions/MaxDepth.java
@@ -3,12 +3,18 @@ package interviewQuestions;
 import TreeNode.TreeNode;
 
 public class MaxDepth {
-
+  /**
+   * maxDepth method to find the depth of the binary tree.
+   * 
+   * @param root of the TreeNode in Binary tree constructed in it's test case.
+   * @return maximum depth of the tree in integer.
+   */
   public int maxDepth(TreeNode root) {
     // if from the root of the tree is null then return 0 since there is no depth of the tree node.
     if (root == null) {
       return 0;
     }
+    // use of Math maximum function to compare depth of the left and right tree and + 1.
     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
   }
 
