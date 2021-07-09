040772451ad494f5c8d9ce5a78114f9a7cec405f
sean112533 <zdte141@live.rhul.ac.uk>
2021-07-09 11:02:49 +0100

    commit another try of Fi23.java

--- JavaPractice/src/ArrayPractice/Fix23.java
+++ JavaPractice/src/ArrayPractice/Fix23.java
@@ -1,8 +1,5 @@
 package ArrayPractice;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /*
  * Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3
  * element to 0. Return the changed array.
@@ -10,6 +7,15 @@ import java.util.List;
  * 
  */
 public class Fix23 {
+
+  public int[] fix23(int[] nums) {
+    for (int i = 0; i < nums.length - 1; i++) {
+      if (nums[i] == 2 && nums[i + 1] == 3) {
+        nums[i + 1] = 0;
+      }
+    }
+    return nums;
+  }
   /**
    * method fix23 to check the following integer array of 2 is 3 is replace to 0.
    * 
@@ -17,23 +23,15 @@ public class Fix23 {
    * @return new replaced number of integers.
    * 
    */
-  public List<Integer> fix23(int[] nums) {
-    // create the arrayList test
-    List<Integer> test = new ArrayList<>();
-    // add the values into the arrayList.
-    for (int temp : nums) {
-      test.add(temp);
-    }
-    // dynamic code to which can implement all length of arrays.
-    for (int i = 0; i < test.size() - 1; i++) {
-      // if the index number contains 2 and following one is 3 then set the 3 to 0.
-      if (test.get(i) == 2 && test.get(i + 1) == 3) {
-
-        test.set(i + 1, 0);
-      }
-    }
-    // return the new arrayList.
-    return test;
-  }
+  /*
+   * public List<Integer> fix23(int[] nums) {
+   * 
+   * // create the arrayList test List<Integer> test = new ArrayList<>(); // add the values into the
+   * arrayList. for (int temp : nums) { test.add(temp); } // dynamic code to which can implement all
+   * length of arrays. for (int i = 0; i < test.size() - 1; i++) { // if the index number contains 2
+   * and following one is 3 then set the 3 to 0. if (test.get(i) == 2 && test.get(i + 1) == 3) {
+   * 
+   * test.set(i + 1, 0); } } // return the new arrayList. return test; }
+   */
 
 }
