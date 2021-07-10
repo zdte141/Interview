e15635131e025fb96147b0562103b2e5c862a381
sean112533 <zdte141@live.rhul.ac.uk>
2021-07-10 18:00:12 +0100

    commit the work with documentation

--- JavaPractice/src/testArrayPractice/TestBiggerTwo.java
+++ JavaPractice/src/testArrayPractice/TestBiggerTwo.java
@@ -7,17 +7,19 @@ import org.junit.Test;
 import ArrayPractice.BiggerTwo;
 
 public class TestBiggerTwo {
+  // TDD to check for the class BiggerTwo.java
   @Before
   public void testClass() {
     BiggerTwo biggerTwo = new BiggerTwo();
   }
 
+  // TDD to check for the method.
   @Test
   public void test() {
     BiggerTwo test = new BiggerTwo();
 
-    int[] a = {1, 2, 3, 4};
-    int[] b = {3, 4, 5, 6};
+    int[] a = {1, 2};
+    int[] b = {3, 4};
 
     assertTrue("compare the two arrays by sum of their number and return the array.",
         Arrays.equals(test.biggerTwo(a, b), b));
